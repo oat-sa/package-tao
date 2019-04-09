dc3aa5fa06e323147660270a8eb055924dac676f
Daniel Halasz <danielh@taotesting.com>
2019-04-09 08:02:12 +0200

    Dockerized community edition

---
+++ bin/SetupGelfLoggerAction.php
@@ -0,0 +1,44 @@
+<?php
+
+use oat\oatbox\action\Action;
+
+class SetGelfLoggerAction implements Action
+{
+    public function __invoke($params)
+    {
+        $fileContent = <<<EOD
+<?php
+
+use Gelf\Publisher;
+use Gelf\Transport\UdpTransport;
+
+return new oat\oatbox\log\LoggerService([
+    'logger' => [
+        'class' => 'oat\\oatbox\\log\\logger\\TaoMonolog',
+        'options' => [
+            'name' => 'tao',
+            'handlers' => [
+                [
+                    'class' => 'Monolog\\Handler\\GelfHandler',
+                    'options' => [
+                        new Publisher(new UdpTransport('oat-docker-logstash', 12201)),
+                        100,
+                    ],
+                ],
+                [
+                    'class' => 'Monolog\\Handler\\StreamHandler',
+                    'options' => [
+                        '/var/www/html/var/log/tao.log',
+                        100,
+                    ],
+                ],
+            ],
+        ],
+    ],
+]);
+
+EOD;
+
+        file_put_contents('config/generis/log.conf.php', $fileContent);
+    }
+}
